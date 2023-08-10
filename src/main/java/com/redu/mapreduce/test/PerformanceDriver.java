ed45cea1cef99f8246b74c618bd27612440feabe
pengzhong <189968924@qq.com>
2023-08-10 10:09:22 +0800

    mapjoin

--- src/main/java/com/redu/mapreduce/test/PerformanceDriver.java
+++ src/main/java/com/redu/mapreduce/test/PerformanceDriver.java
@@ -4,9 +4,12 @@ import cn.hutool.core.date.DatePattern;
 import cn.hutool.core.date.DateUtil;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.orc.OrcConf;
+import org.apache.orc.mapred.OrcStruct;
 import org.apache.orc.mapreduce.OrcOutputFormat;
 
 import java.io.IOException;
@@ -29,6 +32,9 @@ public class PerformanceDriver {
         // 1 获取配置信息以及封装任务
         Configuration configuration = new Configuration();
         configuration.set("paid_month", "2023-06");
+
+        OrcConf.MAPRED_OUTPUT_SCHEMA.setString(configuration,"struct<team_name:string,team_id:int,branch_name:string,branch_id:int,group_name:string,group_id:int,dept_id_path:string,dept_name_path:string,employee_name:string,statistics_time:string,platform:string,order_count:bigint,fund_order_count:bigint,valid_order_num:bigint,gmv:string,fund_order_gmv:string,valid_service_income:string,role_type:int,employee_no:string,order_achievement_sum:string,estimate_service_income:string,user_id:bigint>");
+
         Job job = Job.getInstance(configuration);
         //job.setInputFormatClass(OrcInputFormat.class);
 
@@ -48,8 +54,8 @@ public class PerformanceDriver {
         // 5 设置最终输出kv类型
         //FileOutputFormat.setOutputCompressorClass(job, GzipCodec.class);
         //job.setOutputFormatClass(OrcOutputFormat.class);
-        job.setOutputKeyClass(DimensionVO.class);
-        job.setOutputValueClass(EmployeePerformanceVO.class);
+        job.setOutputKeyClass(NullWritable.class);
+        job.setOutputValueClass(OrcStruct.class);
 
         String ds = LocalDate.now().minusDays(1).format(DatePattern.PURE_DATE_FORMATTER);
 
@@ -61,7 +67,7 @@ public class PerformanceDriver {
 
         // 6 设置输入和输出路径
         //TextInputFormat.setInputPaths(job, new Path("/Users/pengzhong/Downloads/redu_order_tmp__a7368047_80e3_461d_9465_dc3a7dddc71d"));
-        TextInputFormat.setInputPaths(job, new Path("hdfs://hadoop001:9000/user/hive/warehouse/data_cube.db/redu_order_uat/ds=" + ds + "/*"));
+        TextInputFormat.setInputPaths(job, new Path("hdfs://hadoop001:9000/user/hive/warehouse/data_cube.db/redu_order_uat/ds=20230808/*"));
         //FileInputFormat.setInputPaths();
         //OrcInputFormat.setInputPaths(job, new Path("hdfs://hadoop001:9000/user/hive/warehouse/data_cube.db/redu_order/*"));
         //job.setInputFormatClass(OrcInputFormat.class);
--- src/main/java/com/redu/mapreduce/test/PerformanceReducer.java
+++ src/main/java/com/redu/mapreduce/test/PerformanceReducer.java
@@ -1,9 +1,12 @@
 package com.redu.mapreduce.test;
 
+import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONUtil;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.orc.TypeDescription;
+import org.apache.orc.mapred.OrcStruct;
 
 import java.io.IOException;
 import java.math.BigDecimal;
@@ -13,46 +16,135 @@ import java.math.BigDecimal;
  * @since 2023/8/2
  */
 @Slf4j
-public class PerformanceReducer extends Reducer<DimensionVO, EmployeePerformanceVO, NullWritable, EmployeePerformanceVO> {
+public class PerformanceReducer extends Reducer<DimensionVO, EmployeePerformanceVO, NullWritable, OrcStruct> {
 
-    private final EmployeePerformanceVO employeePerformance = new EmployeePerformanceVO();
+    private final TypeDescription schema =
+            TypeDescription.fromString("struct<team_name:string,team_id:int,branch_name:string,branch_id:int,group_name:string,group_id:int,dept_id_path:string,dept_name_path:string,employee_name:string,statistics_time:string,platform:string,order_count:bigint,fund_order_count:bigint,valid_order_num:bigint,gmv:string,fund_order_gmv:string,valid_service_income:string,role_type:int,employee_no:string,order_achievement_sum:string,estimate_service_income:string,user_id:bigint>");
+
+    private final OrcStruct orcStruct = (OrcStruct) OrcStruct.createValue(schema);
+
+    private final Text text01 = new Text();
+
+    private final Text text02 = new Text();
+
+    private final Text text03 = new Text();
+
+    private final Text text04 = new Text();
+
+    private final Text text05 = new Text();
+
+    private final Text text06 = new Text();
+
+    private final Text text07 = new Text();
+
+    private final Text text08 = new Text();
+
+    private final Text text09 = new Text();
+
+    private final Text text10 = new Text();
+
+    private final Text text11 = new Text();
+
+    private final Text text12 = new Text();
+
+    private final Text text13 = new Text();
+
+    private final Text text14 = new Text();
+
+    private final IntWritable intWritable01 = new IntWritable();
+
+    private final IntWritable intWritable02 = new IntWritable();
+
+    private final IntWritable intWritable03 = new IntWritable();
+
+    private final IntWritable intWritable04 = new IntWritable();
+
+    private final LongWritable loneWritable01 = new LongWritable();
+
+    private final LongWritable loneWritable02 = new LongWritable();
+
+    private final LongWritable loneWritable03 = new LongWritable();
+
+    private final LongWritable loneWritable04 = new LongWritable();
 
     @Override
-    protected void reduce(DimensionVO key, Iterable<EmployeePerformanceVO> values, Reducer<DimensionVO, EmployeePerformanceVO, NullWritable, EmployeePerformanceVO>.Context context) throws IOException, InterruptedException {
+    protected void reduce(DimensionVO key, Iterable<EmployeePerformanceVO> values, Reducer<DimensionVO, EmployeePerformanceVO, NullWritable, OrcStruct>.Context context) throws IOException, InterruptedException {
         if (0 == key.getUserId()) {
             return;
         }
+        long orderCount = 0;
+        long fundOrderCount = 0;
+        long validOrderNum = 0;
+        BigDecimal gmv = BigDecimal.ZERO;
+        BigDecimal fundOrderGmv = BigDecimal.ZERO;
+        BigDecimal validServiceIncome = BigDecimal.ZERO;
+        BigDecimal orderAchievementSum = BigDecimal.ZERO;
+        BigDecimal estimateServiceIncome = BigDecimal.ZERO;
         for (EmployeePerformanceVO value : values) {
             try {
-                employeePerformance.setTeamName(value.getTeamName());
-                employeePerformance.setUserId(value.getUserId());
-                employeePerformance.setTeamId(value.getTeamId());
-                employeePerformance.setBranchName(value.getBranchName());
-                employeePerformance.setBranchId(value.getBranchId());
-                employeePerformance.setGroupName(value.getGroupName());
-                employeePerformance.setGroupId(value.getGroupId());
-                employeePerformance.setDeptIdPath(value.getDeptIdPath());
-                employeePerformance.setDeptNamePath(value.getDeptNamePath());
-                employeePerformance.setEmployeeName(value.getEmployeeName());
-                employeePerformance.setStatisticsTime(value.getStatisticsTime());
-                employeePerformance.setPlatform(value.getPlatform());
-                employeePerformance.setOrderCount(employeePerformance.getOrderCount() + value.getOrderCount());
-                employeePerformance.setValidOrderNum(employeePerformance.getValidOrderNum() + value.getValidOrderNum());
-                employeePerformance.setGmv(new BigDecimal(null == employeePerformance.getGmv() ? "0" : employeePerformance.getGmv()).add(new BigDecimal("".equals(value.getGmv()) ? "0" : value.getGmv())).toString());
-                employeePerformance.setFundOrderCount(employeePerformance.getFundOrderCount() + value.getFundOrderCount());
-                employeePerformance.setFundOrderGmv(new BigDecimal(null == employeePerformance.getFundOrderGmv() ? "0" : employeePerformance.getFundOrderGmv()).add(new BigDecimal("".equals(value.getFundOrderGmv()) ? "0" : value.getFundOrderGmv())).toString());
-                employeePerformance.setValidServiceIncome(new BigDecimal(null == employeePerformance.getValidServiceIncome() ? "0" : employeePerformance.getValidServiceIncome()).add(new BigDecimal("".equals(value.getValidServiceIncome()) ? "0" : value.getValidServiceIncome())).toString());
-                employeePerformance.setRoleType(value.getRoleType());
-                employeePerformance.setEmployeeNo(value.getEmployeeNo());
-                employeePerformance.setOrderAchievementSum(new BigDecimal(null == employeePerformance.getOrderAchievementSum() ? "0" : employeePerformance.getOrderAchievementSum()).add(new BigDecimal("".equals(value.getOrderAchievementSum()) ? "0" : value.getOrderAchievementSum())).toString());
-                employeePerformance.setEstimateServiceIncome(new BigDecimal(null == employeePerformance.getEstimateServiceIncome() ? "0" : employeePerformance.getEstimateServiceIncome()).add(new BigDecimal("".equals(value.getEstimateServiceIncome()) ? "0" : value.getEstimateServiceIncome())).toString());
+                text01.set(value.getTeamName());
+                intWritable01.set(value.getTeamId());
+                text02.set(value.getBranchName());
+                intWritable02.set(value.getBranchId());
+                text03.set(value.getGroupName());
+                intWritable03.set(value.getGroupId());
+                text04.set(value.getDeptIdPath());
+                text05.set(value.getDeptNamePath());
+                text06.set(value.getEmployeeName());
+                text07.set(value.getStatisticsTime());
+                text08.set(value.getPlatform());
+                intWritable04.set(value.getRoleType());
+                text12.set(value.getEmployeeNo());
+                loneWritable04.set(value.getUserId());
+
+                orcStruct.setFieldValue(0, text01);
+                orcStruct.setFieldValue(1, intWritable01);
+                orcStruct.setFieldValue(2, text02);
+                orcStruct.setFieldValue(3, intWritable02);
+                orcStruct.setFieldValue(4, text03);
+                orcStruct.setFieldValue(5, intWritable03);
+                orcStruct.setFieldValue(6, text04);
+                orcStruct.setFieldValue(7, text05);
+                orcStruct.setFieldValue(8, text06);
+                orcStruct.setFieldValue(9, text07);
+                orcStruct.setFieldValue(10, text08);
+                orderCount += value.getOrderCount();
+                fundOrderCount += value.getFundOrderCount();
+                validOrderNum += value.getValidOrderNum();
+                gmv = gmv.add(new BigDecimal(StrUtil.isEmpty(value.getGmv()) ? "0" : value.getGmv()));
+                fundOrderGmv = fundOrderGmv.add(new BigDecimal(StrUtil.isEmpty(value.getFundOrderGmv()) ? "0" : value.getFundOrderGmv()));
+                validServiceIncome = validServiceIncome.add(new BigDecimal(StrUtil.isEmpty(value.getValidServiceIncome()) ? "0" : value.getValidServiceIncome()));
+                orderAchievementSum = orderAchievementSum.add(new BigDecimal(StrUtil.isEmpty(value.getOrderAchievementSum()) ? "0" : value.getOrderAchievementSum()));
+                estimateServiceIncome = estimateServiceIncome.add(new BigDecimal(StrUtil.isEmpty(value.getEstimateServiceIncome()) ? "0" : value.getEstimateServiceIncome()));
+
+
+                orcStruct.setFieldValue(17, intWritable04);
+                orcStruct.setFieldValue(18, text12);
+                orcStruct.setFieldValue(21, loneWritable04);
+
             } catch (Exception e) {
                 log.error("PerformanceReducer reduce", e);
                 log.error("PerformanceReducer reduce value:{}", JSONUtil.toJsonStr(value));
-                log.error("PerformanceReducer reduce employeePerformance:{}", JSONUtil.toJsonStr(employeePerformance));
+                log.error("PerformanceReducer reduce employeePerformance:{}", JSONUtil.toJsonStr(value));
             }
         }
-        context.write(NullWritable.get(), employeePerformance);
+        loneWritable01.set(orderCount);
+        orcStruct.setFieldValue(11, loneWritable01);
+        loneWritable02.set(fundOrderCount);
+        orcStruct.setFieldValue(12, loneWritable02);
+        loneWritable03.set(validOrderNum);
+        orcStruct.setFieldValue(13, loneWritable03);
+        text09.set(String.valueOf(gmv));
+        orcStruct.setFieldValue(14, text09);
+        text10.set(String.valueOf(fundOrderGmv));
+        orcStruct.setFieldValue(15, text10);
+        text11.set(String.valueOf(validServiceIncome));
+        orcStruct.setFieldValue(16, text11);
+        text13.set(String.valueOf(orderAchievementSum));
+        orcStruct.setFieldValue(19, text13);
+        text14.set(String.valueOf(estimateServiceIncome));
+        orcStruct.setFieldValue(20, text14);
+        context.write(NullWritable.get(), orcStruct);
     }
 
 }
