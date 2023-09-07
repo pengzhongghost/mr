fadded9da8721279bf2cbd538aa6c1c07d95181b
pengzhong <189968924@qq.com>
2023-09-07 15:59:39 +0800

    人事花名册

--- src/main/java/com/redu/mapreduce/per/PerformanceDriver.java
+++ src/main/java/com/redu/mapreduce/per/PerformanceDriver.java
@@ -25,7 +25,7 @@ public class PerformanceDriver {
 
         // 1 获取配置信息以及封装任务
         Configuration configuration = new Configuration();
-        configuration.set("paid_month", LocalDate.now().minusMonths(3).format(DatePattern.NORM_MONTH_FORMATTER));
+        configuration.set("paid_month", LocalDate.now().minusMonths(2).format(DatePattern.NORM_MONTH_FORMATTER));
 
         OrcConf.MAPRED_OUTPUT_SCHEMA.setString(configuration,"struct<team_name:string,team_id:int,branch_name:string,branch_id:int,group_name:string,group_id:int,dept_id_path:string,dept_name_path:string,employee_name:string,statistics_time:string,platform:string,order_count:bigint,fund_order_count:bigint,valid_order_num:bigint,gmv:string,fund_order_gmv:string,valid_service_income:string,role_type:int,employee_no:string,order_achievement_sum:string,estimate_service_income:string,user_id:bigint,performance_new:string,ds:string,hired_date:string,is_formal:string>");
 
@@ -53,13 +53,16 @@ public class PerformanceDriver {
         job.addCacheFile(new URI("hdfs://hadoop001:9000/user/hive/warehouse/data_cube.db/redu_user/ds=" + ds + "/*"));
         job.addCacheFile(new URI("hdfs://hadoop001:9000/user/hive/warehouse/data_cube.db/user_dept_origin/ds=" + ds + "/*"));
         job.addCacheFile(new URI("hdfs://hadoop001:9000/user/hive/warehouse/data_cube.db/redu_dept/ds=" + ds + "/*"));
+        job.addCacheFile(new URI("hdfs://hadoop001:9000/user/hive/warehouse/data_cube.db/ding_employee/ds=" + ds + "/*"));
         job.addCacheFile(new URI("hdfs://hadoop001:9000/user/hive/warehouse/data_cube.db/config/ds=" + ds + "/*"));
         job.addCacheFile(new URI("hdfs://hadoop001:9000/user/hive/warehouse/data_cube.db/config_item/ds=" + ds + "/*"));
 
         // 6 设置输入和输出路径
-        TextInputFormat.setInputPaths(job, new Path("hdfs://hadoop001:9000/user/hive/warehouse/data_cube.db/redu_order_uat/ds=20230818"));
+        //TextInputFormat.setInputPaths(job, new Path("hdfs://hadoop001:9000/user/hive/warehouse/data_cube.db/redu_order_uat/ds=20230818"));
+        TextInputFormat.setInputPaths(job, new Path("hdfs://hadoop001:9000/user/hive/warehouse/data_cube.db/redu_order/ds=" + ds +"/*"));
         job.setOutputFormatClass(OrcOutputFormat.class);
-        OrcOutputFormat.setOutputPath(job, new Path("hdfs://hadoop001:9000/test/out/performance_temp/" + System.currentTimeMillis()));
+        //OrcOutputFormat.setOutputPath(job, new Path("hdfs://hadoop001:9000/test/out/performance_temp/" + System.currentTimeMillis()));
+        OrcOutputFormat.setOutputPath(job, new Path("hdfs://hadoop001:9000/user/hive/warehouse/data_cube.db/performance_temp/ds=" + ds));
         //第一次排序的
         //job.setSortComparatorClass(MapOutValueComparator.class);
         //FIXME 第二次分组排序的
