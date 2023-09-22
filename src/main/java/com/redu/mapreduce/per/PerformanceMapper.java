33a21d72ac03d2e3ec7b37e6e16e66054e411e29
pengzhong <189968924@qq.com>
2023-09-22 16:47:02 +0800

    二级团长星推

--- src/main/java/com/redu/mapreduce/per/PerformanceMapper.java
+++ src/main/java/com/redu/mapreduce/per/PerformanceMapper.java
@@ -289,7 +289,7 @@ public class PerformanceMapper extends Mapper<LongWritable, Text, DimensionVO, E
             BigDecimal commissionWeight = getPartCommissionWeight(new BigDecimal(String.valueOf(reduOrder.getServiceRate())), outK.getPlatform(), roleType);
             if (null != commissionWeight && StrUtil.isNotEmpty(outV.getValidServiceIncome()) && 0 != BigDecimal.ZERO.compareTo(new BigDecimal(outV.getValidServiceIncome()))) {
                 //如果二级团长是星推团长
-                if (3489157967960523766L == reduOrder.getSecondInstitutionId()) {
+                if (null != reduOrder.getSecondInstitutionId() && 3489157967960523766L == reduOrder.getSecondInstitutionId()) {
                     outV.setPerformanceCommission(outV.getValidServiceIncome());
                 } else {
                     outV.setPerformanceCommission(new BigDecimal(outV.getValidServiceIncome()).multiply(commissionWeight).setScale(3, RoundingMode.FLOOR).toString());
